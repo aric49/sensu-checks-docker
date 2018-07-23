9100a594bd5ed037ea9831349d8caaf0cdc3dc91
Aric Renzo <aricrenzo@gmail.com>
2018-07-23 16:17:35 -0400

    Adding function for deregistering clients.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-
+set -ex
 #Entrypoint Script to dynamically get hostname for the check
+function sensu_cleanup {
+  echo "Deleteing Client......"
+  curl -X DELETE http://sensu-api.internal.tree.com/clients/checks-dockerd-$HOSTNAME_OVERRIDE
+}
 
 export HOSTNAME_OVERRIDE=$ENVIRONMENT-$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 export CLIENT_ADDRESS=$HOSTNAME_OVERRIDE
